1546492c8e365e8bbb7e508ba12630e60ddf0d6c
KornelPralat <162718824+KornelPralat@users.noreply.github.com>
2024-04-05 15:18:57 +0100

    Add files via upload

---
+++ Digi Clock/script.js
@@ -0,0 +1,24 @@
+let hrs = document.getElementById("hrs");
+let min = document.getElementById("min");
+let sec = document.getElementById("sec");
+
+setInterval(() => {
+    let currentTime = new Date();
+
+    let hours = currentTime.getHours();
+    let minutes = currentTime.getMinutes();
+    let seconds = currentTime.getSeconds();
+
+    if (hours < 10) {
+        hours = `0` + hours;
+    }
+    if (minutes < 10) {
+        minutes = `0` + minutes;
+    }
+    if (seconds < 10) {
+        seconds = `0` + seconds;
+    }
+    hrs.innerHTML = hours;
+    min.innerHTML = minutes;
+    sec.innerHTML = seconds;
+},1000)
